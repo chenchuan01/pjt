85a22fba28c12a06fd34d3d0e54ed7059584af5c
chenchuan <329985581@qq.com>
2016-11-22 16:46:00 +0800

    [Modify]目录结构调整

---
+++ pjt-base/src/main/java/com/yidingliu/pjt/base/App.java
@@ -0,0 +1,13 @@
+package com.yidingliu.pjt.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
