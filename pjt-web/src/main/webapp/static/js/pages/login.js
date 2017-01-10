16849bcc9bd28652efc8e41cddce11022eae3f1c
chenchuan <329985581@qq.com>
2017-01-10 10:15:17 +0800

    [UPD] sad

--- pjt-web/src/main/webapp/static/js/pages/login.js
+++ pjt-web/src/main/webapp/static/js/pages/login.js
@@ -1,32 +1,19 @@
-'use strict';
-layui.use(['jquery'],function(){
+layui.use(['form'], function() {
 	window.jQuery = window.$ = layui.jquery;
-	$(function(){
-		layui.use(['form'], function(){
-			  var form = layui.form(),layer = layui.layer;
-			  //自定义验证规则
-			  form.verify({
-			   	pass: [/(.+){6,12}$/, '密码必须6到12位']
-			  });
-			  //监听提交
-			  form.on('submit(sigin)', function(data) {
-				var req = new Request();
-				req.post({
-					url:data.form.action,
-					param:data.field,
-					suc:function(rslt){
-						debugger;
-						if(rslt.code==200){
-							window.location.href=rslt.data;
-						}else{
-							layer.msg(rslt.msg);
-						}
-						
-					}
-				});
-				return false;
-			  });
+	var form = layui.form();
+	 //自定义验证规则
+	form.verify({
+	   pass: [/(.+){6,12}$/, '密码必须6到12位']
+	});
+	form.on('submit(sigin)',function(data){
+		var req = new Request();
+		req.post({
+			url:data.form.action,
+			param:data.field,
+			suc:function(rslt){
+				window.location.href=rslt.data;
+			}
 		});
-		
+		return false;
 	});
 });
